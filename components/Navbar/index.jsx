39f26e5a001b2dcc1cc4fb8f2424ab2e1199337b
Inês Presa <inespresa@MacBook-Air-de-Ines.local>
2022-07-15 01:01:04 +0100

    Make navbar responsive

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,5 +1,12 @@
 import Link from 'next/link'
 
+
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+
 const navigation = [
   { name: "Schedule", slug: "/schedule" },
   { name: "Speakers", slug: "/speakers" },
@@ -11,39 +18,79 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar({current_page}) {
+export default function Navbar({current_page, children}) {
     return (
-      <div className="hidden lg:flex bg-gray1 w-full items-center justify-between border-b border-gray2">
-        <div>
-          Logo
-        </div>
-        <div className="flex gap-x-4 uppercase h-[72px]">
-          {navigation.map((item) => (
-            <Link key={item.slug} href={item.slug}>
+      <Disclosure>
+        {({ open }) => (
+        <>
+          <div className="hidden lg:flex bg-gray1 w-full items-center justify-between border-b border-gray2">
+            <div className="text-white">
+              Logo
+            </div>
+            <div className="flex gap-x-4 uppercase h-[72px]">
+              {navigation.map((item) => (
+                <Link key={item.slug} href={item.slug}>
+                    <a
+                      key={item.name}
+                      href={item.slug}
+                      className={classNames(
+                        current_page == item.name
+                          ? 'border-purple text-white'
+                          : 'border-transparent text-white text-opacity-50 hover:text-opacity-100',
+                        'whitespace-nowrap py-7 px-1 border-b text-sm font-bold'
+                      )}
+                    >
+                    {item.name}
+                  </a>                
+                </Link>
+              ))
+              }
+            </div>
+            <div className="flex gap-x-2 lg:gap-x-4 mr-10">
+              <Link href="https://cesium.di.uminho.pt/">
+                <img class="w-[80px] cursor-pointer opacity-70 hover:opacity-100" src="/images/logos/cesium.svg" />
+              </Link>
+              <Link href="https://necc.di.uminho.pt/">
+                <img class="w-[80px] cursor-pointer opacity-70 hover:opacity-100" src="/images/logos/necc.svg" />
+              </Link>
+            </div>
+          </div>
+          <div className="relative z-50 flex lg:hidden h-[72px] bg-gray1 w-full items-center justify-between border-b border-gray2">
+            <div className="text-white">
+              Logo
+            </div>
+            <Disclosure.Button className="inline-flex h-6 w-6 mr-10 items-center justify-center text-white">
+                {open ? (
+                  <FontAwesomeIcon icon={faTimes} className = 'text-white text-opacity-50 hover:text-opacity-100 whitespace-nowrap py-7 px-1ext-sm font-bold'/>
+                ) : (
+                <FontAwesomeIcon icon={faBars} className = 'text-white text-opacity-50 hover:text-opacity-100 whitespace-nowrap py-7 px-1ext-sm font-bold' />
+                  )}
+            </Disclosure.Button>
+          </div>
+          <Disclosure.Panel className={`lg:hidden bg-gray1 w-full`}>
+            <div className="relative z-50 min-h-screen object-cover px-2 pt-12">
+              {navigation.map((item) => (
+                <Link key={item.slug} href={item.slug}>
                 <a
                   key={item.name}
                   href={item.slug}
                   className={classNames(
                     current_page == item.name
-                      ? 'border-purple text-white'
-                      : 'border-transparent text-white text-opacity-50 hover:text-opacity-100',
-                    'whitespace-nowrap py-7 px-1 border-b text-sm font-bold'
+                      ? ' '
+                      : '  text-opacity-50 hover:text-opacity-100',
+                    'block whitespace-nowrap py-4 px-2 text-center text-xl font-bold uppercase text-white'
                   )}
                 >
                 {item.name}
               </a>                
             </Link>
-          ))
-          }
-        </div>
-        <div className="flex gap-x-2 lg:gap-x-4 mr-10">
-          <Link href="https://cesium.di.uminho.pt/">
-            <img class="w-[80px] cursor-pointer opacity-70 hover:opacity-100" src="/images/logos/cesium.svg" />
-          </Link>
-          <Link href="https://necc.di.uminho.pt/">
-            <img class="w-[80px] cursor-pointer opacity-70 hover:opacity-100" src="/images/logos/necc.svg" />
-          </Link>
-        </div>
-      </div>
+              ))}
+              </div>
+          </Disclosure.Panel>
+          {open ? <div className="hidden lg:block">{children}</div> : children}
+
+        </>
+        )}
+      </Disclosure>
     );
 }
