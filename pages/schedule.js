79d0c7329f68436c9b48cedc25203c52111dadcf
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-08-25 14:45:16 +0100

    Add CI style workflow

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-import Navbar from '/components/Navbar'
-import DefaultPhoto from '/components/DefaultPhoto'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import Navbar from "/components/Navbar";
+import DefaultPhoto from "/components/DefaultPhoto";
 
-import schedule from '/data/schedule.json'
+import schedule from "/data/schedule.json";
 
-export default function Schedule(){
+export default function Schedule() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
@@ -17,74 +17,87 @@ export default function Schedule(){
       <Navbar current_page="Schedule" />
 
       <main className="flex w-full flex-1 flex-col bg-black my-auto">
-        
         <div className="px-10 mb-10">
           <p className="text-white text-[40px] mt-14 font-extrabold">
             Schedule
           </p>
           <p className="text-gray3 text-md mt-4">
-            This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.
+            This is an open source demo that Next.js developers can clone,
+            deploy, and fully customize for events. Created through
+            collaboration of marketers, designers, and developers at Vercel.
           </p>
         </div>
 
         {schedule.map((day, i0) => (
           <div className="sm:flex border-t border-gray2 sm:py-4" key={day}>
             <div className="px-10 py-4 sm:py-0 sm:p-10 flex-shrink-0 sm:mr-4">
-              <p className="text-purple text-4xl font-extrabold">
-                {day.date}
-              </p>
+              <p className="text-purple text-4xl font-extrabold">{day.date}</p>
             </div>
             <div className="px-10 overflow-x-auto flex gap-4 pb-4 sm:pb-0 sm:py-4">
               {day.activities.map((activity, i1) => (
-                <div className="flex flex-col" key={activity}> 
-                  <div className="text-gray3 text-md"> 
-                     {activity.start} - {activity.end}
-                  </div> 
-                  <> 
-                  <Link href={`/activity/${i1}?day_id=${i0}`} >
-                    <div className="cursor-pointer group w-80 mt-2 relative rounded-lg border border-gray-300 bg-gray1 border-gray2 px-6 py-5 shadow-sm hover:border-white">
-                      <div className="flex justify-between items-center">
-                        <p className="text-white text-xl font-bold"> 
-                          {activity.title}
-                        </p>
-                        { activity.emoji}
-                      </div> 
-                      <div className="mt-4 text-white text-sm text-opacity-40">
-                        {activity.location}
-                      </div>
-                      { activity.speakers &&
-
-                        ( activity.speakers.length > 1
-                            &&
-                            <>        
-                            <div className="flex group-hover:hidden items-center -space-x-5 mt-4">
-                              {activity.speakers.map((speaker) => (
-                                  <div className="flex-shrink-0" key={speaker}>
-                                    { speaker.photo &&
-                                    <div className="rounded-3xl overflow-hidden">
-                                      <Image src={`/images/speakers/${speaker.photo}`} alt={speaker.name} width={50} height={50} layout="fixed"/>
-                                    </div>                                       
-                                    ||
-                                    <DefaultPhoto name={speaker.name}/>
-                                    }
-                                  </div>
-                              ))}
-                            </div>
-                            <div className="hidden group-hover:block items-center mt-4">
+                <div className="flex flex-col" key={activity}>
+                  <div className="text-gray3 text-md">
+                    {activity.start} - {activity.end}
+                  </div>
+                  <>
+                    <Link href={`/activity/${i1}?day_id=${i0}`}>
+                      <div className="cursor-pointer group w-80 mt-2 relative rounded-lg border border-gray-300 bg-gray1 border-gray2 px-6 py-5 shadow-sm hover:border-white">
+                        <div className="flex justify-between items-center">
+                          <p className="text-white text-xl font-bold">
+                            {activity.title}
+                          </p>
+                          {activity.emoji}
+                        </div>
+                        <div className="mt-4 text-white text-sm text-opacity-40">
+                          {activity.location}
+                        </div>
+                        {activity.speakers &&
+                          ((activity.speakers.length > 1 && (
+                            <>
+                              <div className="flex group-hover:hidden items-center -space-x-5 mt-4">
                                 {activity.speakers.map((speaker) => (
-                                  <div className="flex items-center space-x-3 mt-4" key={speaker}>
+                                  <div className="flex-shrink-0" key={speaker}>
+                                    {(speaker.photo && (
+                                      <div className="rounded-3xl overflow-hidden">
+                                        <Image
+                                          src={`/images/speakers/${speaker.photo}`}
+                                          alt={speaker.name}
+                                          width={50}
+                                          height={50}
+                                          layout="fixed"
+                                        />
+                                      </div>
+                                    )) || <DefaultPhoto name={speaker.name} />}
+                                  </div>
+                                ))}
+                              </div>
+                              <div className="hidden group-hover:block items-center mt-4">
+                                {activity.speakers.map((speaker) => (
+                                  <div
+                                    className="flex items-center space-x-3 mt-4"
+                                    key={speaker}
+                                  >
                                     <div className="flex-shrink-0">
-                                      { speaker.photo &&
+                                      {(speaker.photo && (
                                         <div className="rounded-3xl overflow-hidden">
-                                          <Image src={`/images/speakers/${speaker.photo}`} alt={speaker.name} width={50} height={50} layout="fixed"/>
-                                        </div>                                          
-                                        ||
-                                        <DefaultPhoto name={speaker.name}/>
-                                      }
+                                          <Image
+                                            src={`/images/speakers/${speaker.photo}`}
+                                            alt={speaker.name}
+                                            width={50}
+                                            height={50}
+                                            layout="fixed"
+                                          />
+                                        </div>
+                                      )) || (
+                                        <DefaultPhoto name={speaker.name} />
+                                      )}
                                     </div>
                                     <div className="flex-1 min-w-0">
                                       <div className="focus:outline-none">
-                                        <span className="absolute inset-0" aria-hidden="true" />
+                                        <span
+                                          className="absolute inset-0"
+                                          aria-hidden="true"
+                                        />
                                         <p className="text-sm font-medium text-white opacity-75">
                                           {speaker.name}
                                         </p>
@@ -94,45 +107,53 @@ export default function Schedule(){
                                       </div>
                                     </div>
                                   </div>
-                              ))}
-                            </div>
-                            </>
-                        ||
-                        activity.speakers.map((speaker) => (
-                          <div className="flex items-center space-x-3 mt-4" key={speaker}>
-                            <div className="flex-shrink-0">
-                              { speaker.photo &&
-                                <div className="rounded-3xl overflow-hidden">
-                                  <Image src={`/images/speakers/${speaker.photo}`} alt={speaker.name} width={50} height={50} layout="fixed"/>
-                                </div>    
-                                ||
-                                <DefaultPhoto name={speaker.name}/>
-                              }
-                            </div>
-                            <div className="flex-1 min-w-0">
-                              <div className="focus:outline-none">
-                                <span className="absolute inset-0" aria-hidden="true" />
-                                <p className="text-sm font-medium text-white opacity-75">
-                                  {speaker.name}
-                                </p>
-                                <p className="text-sm text-gray3 opacity-70 truncate">
-                                  {speaker.role}
-                                </p>
+                                ))}
                               </div>
-                            </div>
-                          </div>)
-                      ))}
-                    </div>
-                  </Link>
+                            </>
+                          )) ||
+                            activity.speakers.map((speaker) => (
+                              <div
+                                className="flex items-center space-x-3 mt-4"
+                                key={speaker}
+                              >
+                                <div className="flex-shrink-0">
+                                  {(speaker.photo && (
+                                    <div className="rounded-3xl overflow-hidden">
+                                      <Image
+                                        src={`/images/speakers/${speaker.photo}`}
+                                        alt={speaker.name}
+                                        width={50}
+                                        height={50}
+                                        layout="fixed"
+                                      />
+                                    </div>
+                                  )) || <DefaultPhoto name={speaker.name} />}
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                  <div className="focus:outline-none">
+                                    <span
+                                      className="absolute inset-0"
+                                      aria-hidden="true"
+                                    />
+                                    <p className="text-sm font-medium text-white opacity-75">
+                                      {speaker.name}
+                                    </p>
+                                    <p className="text-sm text-gray3 opacity-70 truncate">
+                                      {speaker.role}
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            )))}
+                      </div>
+                    </Link>
                   </>
                 </div>
               ))}
             </div>
           </div>
         ))}
-
       </main>
-
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
-      transparent: 'transparent',
-      'white': '#ffffff',
-      'black': '#000000',
-      'gray1': '#0e0e10',
-      'gray2': '#333333',
-      'gray3': '#8a8f98',
-      'purple': '#845ef7',
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#000000",
+      gray1: "#0e0e10",
+      gray2: "#333333",
+      gray3: "#8a8f98",
+      purple: "#845ef7",
     },
     extend: {},
   },
   plugins: [],
-}
+};
