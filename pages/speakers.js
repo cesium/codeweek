feffa5ec47949d052891ff7a83277df52589ba0a
Rui Oliveira <rpoliveira02@gmail.com>
2022-10-09 18:38:05 +0100

    Fix div size

--- pages/speakers.js
+++ pages/speakers.js
@@ -28,7 +28,7 @@ export default function Speakers() {
           <div className="px-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 2xl:grid-cols-7 gap-2">
             {speakers.map((speaker, i) => (
               <Link href={`/speaker/${i}`} key={speaker}>
-                <div className="space-y-4 cursor-pointer group relative rounded-lg border border-gray-300 bg-gray1 border-gray2 shadow-sm hover:border-purple">
+                <div className="space-y-4 cursor-pointer group relative rounded-lg border border-gray-300 bg-gray1 border-gray2 shadow-sm hover:border-purple w-[220px]">
                   <div className="aspect-w-3 aspect-h-2">
                     <div className="relative overflow-hidden h-[220px]">
                       {/* eslint-disable @next/next/no-img-element */}
