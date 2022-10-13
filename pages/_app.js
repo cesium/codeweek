d146ee01f98dc75f93f5b4da9d64543584d26809
Rui Lopes <rui.lilokas2003@hotmail.com>
2022-10-13 17:02:02 +0100

    Fix fontawesome icons (#47)

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
